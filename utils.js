343bc71bb9797705fa7acec73e17e6e851f2b812
awarestudio <awarestudio.cc@gmail.com>
2023-04-04 09:24:14 +0200

    Added linspace function

--- utils.js
+++ utils.js
@@ -1,7 +1,22 @@
-function range(start, end, step) {
+function range(start, stop, step) {
     let arr = []
-    for (let iteration=start; iteration<end; iteration+=step) {
+    for (let iteration = start; iteration < stop; iteration += step) {
         arr.push(iteration)
     }
     return arr
+}
+
+function linspace(start, stop, num, endPoint=true, retstep=false) {
+    let arr = []
+    let correction = endPoint ? 1 : 0
+    let step = (stop - start) / (num - correction)
+    for (let iteration=0; iteration < num; iteration++) {
+        arr.push(start + iteration*step)
+    }
+    if (retstep) {
+        return [arr, step]
+    }
+    else {
+        return arr
+    }
 }
