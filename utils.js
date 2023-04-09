a023a49bdbafbcb62152a7a7bd60537d17233dfd
awarestudio <awarestudio.cc@gmail.com>
2023-04-09 19:43:02 +0200

    Added save function

--- utils.js
+++ utils.js
@@ -27,3 +27,14 @@ function drawDebugLines(layer, debugColor="magenta", debugWeight=1) {
     layer.rect(0, 0, layer.width, layer.height)
     layer.line(0, 0, layer.width, layer.height)
 }
+
+function keyTyped() {
+    if (key == 's') {
+        saveImage()
+    }
+}
+
+function saveImage(extension='png'){
+    timestamp = urlPath[-1] + "_" + year() + "_" + month() + "_" + day() + "_" + hour() + "_" + minute() + "_" + second() + "_" + nf(millis(), 3, 0)
+    save(timestamp + extension)
+}
