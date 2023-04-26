038ee21cc3b712933514b2cdabb0844238ea5f1b
awarestudio <awarestudio.cc@gmail.com>
2023-04-26 10:07:16 +0200

    Removed comments

--- utils.js
+++ utils.js
@@ -40,19 +40,6 @@ function randomWeighted(items, weights) {
         }
     }
     return items[index]
-    
-    // var i;
-
-    // for (i = 1; i < weights.length; i++)
-    //     weights[i] += weights[i - 1];
-    
-    // var random = Math.random() * weights[weights.length - 1];
-    
-    // for (i = 0; i < weights.length; i++)
-    //     if (weights[i] > random)
-    //         break;
-    
-    // return items[i];
 }
 
 function drawDebugLines(layer, debugColor="magenta", debugWeight=1) {
