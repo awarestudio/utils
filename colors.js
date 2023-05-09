19bdf63c6de60d1c37e281a349a2de4b5be2c96f
awarestudio <awarestudio.cc@gmail.com>
2023-05-09 22:08:44 +0200

    Added ColorMap fromInversion constructor

--- colors.js
+++ colors.js
@@ -1,8 +1,24 @@
 class ColorMap {
-	constructor(colors, intervals, lerp=false) {
+	constructor(colors, intervals, interpolation=false) {
 		this.colors = colors
 		this.intervals = intervals
-		this.lerp = lerp
+		this.interpolation = interpolation
+	}
+
+	static fromInversion(cm) {
+		let colorMap = new ColorMap([], cm.intervals, cm.interpolation)
+
+		for (let colorIndex = 0; colorIndex < cm.colors.length; colorIndex++) {	
+			let c = color(0, 0, 0, cm.colors[colorIndex].levels[3])
+			
+			for (let levelIndex = 0; levelIndex < c.levels.length - 1; levelIndex++) {
+				c.levels[levelIndex] = 255 - cm.colors[colorIndex].levels[levelIndex]
+			}
+			
+			colorMap.colors.push(c)
+		}
+
+		return colorMap
 	}
 
 	getColor(color_value) {
@@ -13,16 +29,16 @@ class ColorMap {
 			let right_interval_value = this.intervals[interval_index + 1]
 
 			if (color_value < right_interval_value) {
-				if (this.lerp) {
+				if (this.interpolation) {
 					let lerp_value = map(color_value, left_interval_value, right_interval_value, 0, 1)
 
-					if (this.lerp == 'linear') {
+					if (this.interpolation == 'linear') {
 						final_color = lerpColor(
 							this.colors[interval_index],
 							this.colors[interval_index+1],
 							lerp_value
 						)
-					} else if (this.lerp == 'quadratic') {
+					} else if (this.interpolation == 'quadratic') {
 						final_color = ColorMap.quadraticLerpColor(
 							this.colors[interval_index],
 							this.colors[interval_index+1],
@@ -48,6 +64,5 @@ class ColorMap {
 			quadraticInterpolation(c1.levels[3], c2.levels[3], v),
 		)
 	}
-
 }
 
